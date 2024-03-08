dd22485b8795daba6a738b12c6ed45bcaf176210
victor langat <provicml@gmail.com>
2024-03-08 23:06:26 +0300

    add db

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,37 @@
-#!/usr/bin/node
+// import mongodb from 'mongodb';
+const { MongoClient } = require('mongodb');
+// import { MongoClient } from 'mongodb/lib/mongo_client';
+// import Collection from 'mongodb/lib/collection';
+// import envLoader from './env_loader';
+
+class DBClient {
+  constructor () {
+    // envLoader();
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const dbName = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${dbName}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  // returns true when the connection to MongoDB is a success otherwise, false
+  isAlive () {
+    return this.client.isConnected();
+  }
+
+  // returns the number of documents in the collection users
+  async nbUsers () {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  // returns the number of documents in the collection files
+  async nbFiles () {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+// create and export an instance of DBClient called dbClient
+export const dbClient = new DBClient();
+export default dbClient;
