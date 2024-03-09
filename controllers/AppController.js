f48481af731e7e286bff8f07af4edc1b1aac9af2
victor langat <provicml@gmail.com>
2024-03-09 11:41:16 +0300

    test workdflow

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,5 +1,5 @@
-import {dbClient} from '../utils/db';
-import {redisClient} from '../utils/redis';
+import { dbClient } from '../utils/db';
+import { redisClient } from '../utils/redis';
 
 class AppController {
   static status(req, res) {
