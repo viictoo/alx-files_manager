b08b6de9ff143dbd227e6cfd4eb57e453e88ee05
victor langat <provicml@gmail.com>
2024-03-09 10:23:45 +0300

    fix eslint error

--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,14 @@
 // import mongodb from 'mongodb';
 const { MongoClient } = require('mongodb');
-const process = require('process')
 // import { MongoClient } from 'mongodb/lib/mongo_client';
 // import Collection from 'mongodb/lib/collection';
 // import envLoader from './env_loader';
 
 class DBClient {
-  constructor () {
+  constructor() {
     // envLoader();
     const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
+    const port = +(process.env.DB_PORT) || 27017;
     const dbName = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}/${dbName}`;
 
@@ -18,18 +17,28 @@ class DBClient {
   }
 
   // returns true when the connection to MongoDB is a success otherwise, false
-  isAlive () {
+  isAlive() {
     return this.client.isConnected();
   }
 
   // returns the number of documents in the collection users
-  async nbUsers () {
+  async nbUsers() {
     return this.client.db().collection('users').countDocuments();
   }
 
   // returns the number of documents in the collection files
-  async nbFiles () {
+  async nbFiles() {
     return this.client.db().collection('files').countDocuments();
+  }
+
+  // Retrieves a reference to the `users` collection.
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
+
+  //  Retrieves a reference to the `files` collection.
+  async filesCollection() {
+    return this.client.db().collection('files');
   }
 }
 
