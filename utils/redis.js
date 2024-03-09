10f836b8db2a33fe10eb6d9dbf01e79f12ed8cfb
victor langat <provicml@gmail.com>
2024-03-09 10:21:15 +0300

    fix eslint errors

--- utils/redis.js
+++ utils/redis.js
@@ -4,18 +4,23 @@ Inside the folder utils, create a file redis.js that contains the class RedisCli
 RedisClient should have:
 
 the constructor that creates a client to Redis:
-any error of the redis client must be displayed in the console (you should use on('error') of the redis client)
-a function isAlive that returns true when the connection to Redis is a success otherwise, false
-an asynchronous function get that takes a string key as argument and returns the Redis value stored for this key
-an asynchronous function set that takes a string key, a value and a duration in second as arguments to store it in Redis (with an expiration set by the duration argument)
-an asynchronous function del that takes a string key as argument and remove the value in Redis for this key
-After the class definition, create and export an instance of RedisClient called redisClient.
+any error of the redis client must be displayed in
+the console (you should use on('error') of the redis client)
+a function isAlive that returns true when the connection
+to Redis is a success otherwise, false
+an asynchronous function get that takes a string key as
+argument and returns the Redis value stored for this key
+an asynchronous function set that takes a string key,
+a value and a duration in second as arguments to store it in Redis
+(with an expiration set by the duration argument)
+an asynchronous function del that takes a string key as argument
+and remove the value in Redis for this key
+After the class definition, create and export an instance of
+RedisClient called redisClient.
 */
-
 
 import { promisify } from 'util';
 import { createClient } from 'redis';
-
 
 class RedisClient {
   constructor() {
@@ -40,14 +45,14 @@ class RedisClient {
     return promisify(this.client.GET).bind(this.client)(key);
   }
 
-  // 
+  // that takes a string key, a value and a duration in second as arguments to store it in Redi
   async set(key, value, duration) {
     await promisify(this.client.SETEX)
       .bind(this.client)(key, duration, value);
   }
 
   /*
- takes a string key, a value and a duration in second 
+ takes a string key, a value and a duration in second
  as arguments to store it in Redis
  (with an expiration set by the duration argument)
    */
