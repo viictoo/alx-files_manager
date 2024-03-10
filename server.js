1759cc0d9244e2ef30ad3088992026ab95f6f82f
victor langat <provicml@gmail.com>
2024-03-10 08:31:10 +0300

    task 3 update server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use('/', router);
 app.listen(PORT, () => {
-  console.log(`App is litening on Port ${5000}`);
+  console.log(`App is listening on Port ${PORT}`);
 });
 
 module.exports = app;
