ceb2b1383212e94bed9bcba3aeca953f4e417fa2
victor langat <provicml@gmail.com>
2024-03-11 00:47:15 +0300

    test fix task 4 router

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,19 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
-
 router.get('/stats', AppController.getStats);
 
 router.post('/users', UsersController.postNew);
-
 router.get('/users/me', UsersController.getMe);
 
 router.get('/connect', AuthController.getConnect);
-
 router.get('/disconnect', AuthController.getDisconnect);
+
+router.post('/files', FilesController.postUpload);
 
 export default router;
