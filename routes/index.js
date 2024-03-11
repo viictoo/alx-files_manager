58cf645557bf65aea83983f58d5eb14e9713d454
victor langat <provicml@gmail.com>
2024-03-11 19:43:56 +0300

    update routes: (un)publish

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,7 @@ router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
 export default router;
